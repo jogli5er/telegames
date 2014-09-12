b51820a504b2091d47f78fee740e902191d050a7
Martin Vögeli <github@birdy1976.com>
2014-09-12 17:07:34 +0200

    The dawn of the game logic...

--- backend/src/Hackathon/Bundle/GameBundle/GameLogic/ConnectFour.php
+++ backend/src/Hackathon/Bundle/GameBundle/GameLogic/ConnectFour.php
@@ -1,16 +1,42 @@
 <?php
 
-namespace Hackathon\Bundle\GameBundle\Entity;
+namespace Hackathon\Bundle\GameBundle\GameLogic;
 
 use Hackathon\Bundle\GameBundle\Entity\Game;
+use Hackathon\Bundle\GameBundle\Entity\GameTurn;
 
 /**
  * Gamelogic for ConnectFour
  */
 class ConnectFour
 {
-    public function __construct(Game $game) {
+    public $game;
+    public $width  = 7;
+    public $height = 6;
+    public $grid;
+    // e.g. 7x6: [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]];
 
+    public function __construct(Game $game) {
+        $this->game = $game;
+        for ($i = 0; $i < $this->width; $i++) {
+            for ($j = 0; $j < $this->height; $j++) {
+                $this->grid[$i][$j] = 0;
+            }
+        }
+    }
+
+    public function getOptions() {
+        $options;
+        for ($i = 0; $i < $this->width; $i++) {
+            if($this->grid[$i][$this->height-1] == 0){
+                $options[] = $i;
+            }
+        }
+        return $options;
+    }
+
+    public function addMoves() {
+        $test = $this->game->getTurns();
+        return $test[0]->getMove();
     }
 }
-
