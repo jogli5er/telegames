b591e93596b6401556769196ab02c85b7a6489be
Sandro Meier <sandro.meier@fidelisfactory.ch>
2014-09-12 14:53:02 +0200

    Add game logic for ConnectFour

--- backend/src/Hackathon/Bundle/GameBundle/GameLogic/ConnectFour.php
+++ backend/src/Hackathon/Bundle/GameBundle/GameLogic/ConnectFour.php
@@ -2,11 +2,15 @@
 
 namespace Hackathon\Bundle\GameBundle\Entity;
 
+use Hackathon\Bundle\GameBundle\Entity\Game;
+
 /**
  * Gamelogic for ConnectFour
  */
 class ConnectFour
 {
+    public function __construct(Game $game) {
 
+    }
 }
 
