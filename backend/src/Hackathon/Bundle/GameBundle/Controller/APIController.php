9213cc2d0cbb41f40fac510f305467aa5b530ddf
Sandro Meier <sandro.meier@fidelisfactory.ch>
2014-09-12 17:57:43 +0200

    implement updateMoveAction

--- backend/src/Hackathon/Bundle/GameBundle/Controller/APIController.php
+++ backend/src/Hackathon/Bundle/GameBundle/Controller/APIController.php
@@ -68,6 +68,49 @@ class APIController extends Controller
 	$entityManager = $this->getDoctrine()->getManager();
 	$repo = $entityManager->getRepository("HackathonGameBundle:Game");
 	$game = $repo->findCurrentGame();
+
+	$data = $this->createMoveData($game);
+
+	return $this->createObjectResponse($data);
+    }
+
+    /**
+     * @Route("game/move/user/{id}")
+     * @ParamConverter("user", class="HackathonGameBundle:User")
+     * @Method({"POST"})
+     */
+    public function updateMoveAction(User $user)
+    {
+	$request = $this->getRequest();
+	$bodyContent = $request->getContent();
+
+	// Get the number and check range
+	$selectedOption = intval($bodyContent);
+
+	/// @todo Check if selected option is valid
+	
+	// Set selection and store
+	$user->setSelection($selectedOption);
+	$entityManager = $this->getDoctrine()->getManager();
+	$entityManager->flush();
+
+	// Return the expected data
+	return $this->createObjectResponse($this->createMoveData());
+    }
+
+    /**
+     * Creates the data array of moves. See the README on what is expected
+     *
+     * If game is not specified, the current game is used
+     */
+    protected function createMoveData($game = NULL)
+    {
+	if ($game == NULL) {
+	    $entityManager = $this->getDoctrine()->getManager();
+	    $repo = $entityManager->getRepository("HackathonGameBundle:Game");
+	    $game = $repo->findCurrentGame();
+	}
+
 	$gameLogic = new ConnectFour($game);
 
 	$options = $gameLogic->getOptions();
@@ -87,16 +130,6 @@ class APIController extends Controller
 	    "moves" => $formattedOptions
 	);
 
-	return $this->createObjectResponse($data);
-    }
-
-    /**
-     * @Route("game/move/user/{id}")
-     * @ParamConverter("user", class="HackathonGameBundle:User")
-     * @Method({"POST"})
-     */
-    public function updateMoveAction(User $user)
-    {
-
+	return $data;
     }
 }
