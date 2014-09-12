190a21351d5ccdb2616da72e7f784185c5fbcf09
Sandro Meier <sandro.meier@fidelisfactory.ch>
2014-09-12 20:21:03 +0200

    Add logging

--- backend/src/Hackathon/Bundle/GameBundle/Command/GameManagerCommand.php
+++ backend/src/Hackathon/Bundle/GameBundle/Command/GameManagerCommand.php
@@ -29,7 +29,10 @@ class GameManagerCommand extends ContainerAwareCommand
 	if ($timeTillRoundFinished <= 0) {
 	    // The round ended! Let's start a new one!
 	    $this->startNewTurn();
+	    echo "Starting new round";
 	}
+
+	echo "Ended update\n";
     }
 }
 
